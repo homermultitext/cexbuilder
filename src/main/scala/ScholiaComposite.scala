1190f6aa2f8c6241920dace8ec335822c32918b8
Neel Smith <dnsmith.neel@gmail.com>
2018-03-21 06:35:02 -0400

    Extract scholia doc from multiple xml sources

--- src/main/scala/ScholiaComposite.scala
+++ src/main/scala/ScholiaComposite.scala
@@ -30,4 +30,32 @@ object ScholiaComposite {
     allGroups.flatten.map(_.attribute("n").get.text).toSet
   }
 
+
+  /** Extract a given scholia document from a set of XML
+  * source files and create a single composite text (as a String).
+  *
+  * @param document Document identifier for scholia document (work ID in the document's CTS URN).
+  * @param files XML files to extract content from.
+  */
+  def compositeDocument(document : String, files: Set[File]): String = {
+    val lines = for (f <- files) yield {
+      val root = XML.loadFile(f)
+      val teiGroup = root \ "text" \ "group"
+      val bkNode = teiGroup(0)
+      val book = bkNode.attribute("n").get
+      val bookOpen = s"""<div n="${book}" type="book">"""
+
+      val allDocs = bkNode \ "text"
+      val relevant = allDocs.filter(_.attribute("n").get.text == document)
+      if (relevant.size > 0) {
+        val doc = relevant(0)
+        val scholia = doc \ "body" \ "div"
+        println("\tIn " + document + "book " + book  +", " + scholia.size + " scholia.")
+        val scholStrings = scholia.map(_.toString)
+        bookOpen + scholStrings.mkString("\n") + "</div>"
+      } else { "" }
+    }
+    lines.mkString("\n")
+  }
+
 }
--- src/test/scala/ScholiaCompositeSpec.scala
+++ src/test/scala/ScholiaCompositeSpec.scala
@@ -2,6 +2,7 @@ package org.homermultitext.cexbuilder
 
 import org.scalatest.FlatSpec
 import java.io.File
+import scala.xml._
 
 class ScholiaCompositeSpec extends FlatSpec {
 
@@ -10,5 +11,23 @@ class ScholiaCompositeSpec extends FlatSpec {
     val expected = Set("msA",
     "msAextra", "msAext", "msAil", "msAint", "msAim", "msAlater")
     assert(ScholiaComposite.scholiaSet(scholiaSrc) == expected)
+  }
+
+
+
+  it should "extract text content for a given single document" in {
+    val doc = "msAextra"
+    val srcDir = "src/test/resources/scholia-xml"
+    val xmlFiles = filesInDir(srcDir, "xml")
+    val msAextraText = ScholiaComposite.compositeDocument(doc, xmlFiles)
+    // should produce two scholia in book 1 and one in book 2:
+    val root = XML.loadString("<root>" + msAextraText + "</root>")
+    val books = root \ "div"
+    assert(books.size == 2)
+    val book1 = books(0)
+    val scholia1 = book1 \ "div"
+    assert (scholia1.size == 2)
+    val scholia2 = books(1)
+    assert (scholia2.size == 1)
   }
 }
